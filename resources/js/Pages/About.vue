c73f82d62e2c2e026a205d9dfdf14966fe3d3516
WaiYanLin71 <scm.waiyanlinn@gmail.com>
2023-05-16 15:21:17 +0630

    initial

---
+++ resources/js/Pages/About.vue
@@ -0,0 +1,16 @@
+
+
+<template>
+  <div>
+    <Link href="/">Home</Link>
+    <Link href="/about">About</Link>
+  </div>
+</template>
+
+<script setup>
+import { Link } from '@inertiajs/vue3'
+</script>
+
+<style lang="scss" scoped>
+
+</style>
